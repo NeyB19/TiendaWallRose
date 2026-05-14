a834bd56b6258f8bbe5b970e5d73d0c5fb56b9c0
NeyB19 <160780976+NeyB19@users.noreply.github.com>
2026-05-13 22:05:26 -0600

    Clase Linea completa

--- TiendaWallRose/src/logica/Linea.java
+++ TiendaWallRose/src/logica/Linea.java
@@ -8,4 +8,47 @@ public class Linea {
         this.producto = producto;
         this.cantidad = cantidad;
     }
+
+
+    public double calcularCosto() {
+        double costoTotal = 0;
+        if (this.producto != null) {
+            costoTotal = this.producto.getPrecio() * this.cantidad;
+        }
+        return costoTotal;
+    }
+
+    public double getCantidad() {
+        return this.cantidad;
+    }
+
+    public void setCantidad(double cantidad) throws Exception {
+        if (cantidad > 0) {
+            this.cantidad = cantidad;
+            return;
+        }
+        throw new Exception("Error: La cantidad debe ser mayor a cero.");
+    }
+
+    public Producto getProducto() {
+        return this.producto;
+    }
+
+    public void setProducto(Producto producto) throws Exception {
+        if (producto != null) {
+            this.producto = producto;
+            return;
+        }
+        throw new Exception("Error: Se debe asignar un producto.");
+    }
+
+ 
+    public String toString() {
+        return "\n  DETALLE DE LÍNEA " +
+               "\nProducto: " + this.producto.getNombre() +
+               "\nCantidad: " + cantidad +
+               "\nPrecio unitario: " + this.producto.getPrecio() +
+               "\nCosto total: " + calcularCosto() +
+               "\n----------------------------";
+    }
 }
